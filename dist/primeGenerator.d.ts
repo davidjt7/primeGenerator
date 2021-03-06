bba4ba58361a8e81ccdcc3858f791b94d13ec241
David Thomas <djt.in7@gmail.com>
2021-03-06 22:33:41 +0530

    Added d.ts files

---
+++ dist/primeGenerator.d.ts
@@ -0,0 +1,4 @@
+export function getPrimes(start: any, end: any, primeCheck: any): any[];
+export function isPrimeNaive(n: any): boolean;
+export function isPrimeOptimized(n: any): boolean;
+export function sieveOfEratosthenes(start: any, end: any): any[];
